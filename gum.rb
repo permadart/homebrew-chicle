2e5ed44276b141f0348789d373e0df8dc53939fa
Esteban Dart <esteban@permad.art>
2024-08-20 11:19:29 +0200

    Update sha256

--- gum.rb
+++ gum.rb
@@ -2,7 +2,7 @@ class Gum < Formula
     desc "Git User Manager - Platform-agnostic tool for managing multiple Git identities"
     homepage "https://github.com/permadart/gum"
     url "https://github.com/permadart/gum/archive/refs/tags/v0.0.1+3.tar.gz"
-    sha256 "a65e48422f50bfa046c9d58e2231b2796a017b0cd1c01647e40adbfaf70ce4df"
+    sha256 "b5f3e0afd22d1dc03f2365de618a569c8fda1b52175e98a296caac7172a9c3dc"
     license "MIT"
 
     depends_on "go" => :build
