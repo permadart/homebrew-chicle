44e80a4ad76fe54eed70cc293f7f1f4ec33d980c
Esteban Dart <esteban@permad.art>
2024-08-20 11:35:32 +0200

    Update v0.0.1+4

--- gum.rb
+++ gum.rb
@@ -1,7 +1,7 @@
 class Gum < Formula
     desc "Git User Manager - Platform-agnostic tool for managing multiple Git identities"
     homepage "https://github.com/permadart/gum"
-    url "https://github.com/permadart/gum/archive/refs/tags/v0.0.1+3.tar.gz"
+    url "https://github.com/permadart/gum/archive/refs/tags/v0.0.1+4.tar.gz"
     sha256 "b5f3e0afd22d1dc03f2365de618a569c8fda1b52175e98a296caac7172a9c3dc"
     license "MIT"
 
