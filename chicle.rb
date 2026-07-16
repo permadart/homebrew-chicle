2c4a036c2139715419a69a897ba0aa6b1218350b
Esteban Dart <herrmartellgmail.com>
2026-07-16 12:43:38 +0200

    Add explicit version field to formula

--- chicle.rb
+++ chicle.rb
@@ -3,6 +3,7 @@ class Chicle < Formula
     homepage "https://github.com/permadart/chicle"
     url "https://github.com/permadart/chicle/archive/refs/tags/v0.0.4+1.tar.gz"
     sha256 "3970194a06be930c758702b51dc0f2a7bc62ff8e323350bc47634544c1f9045d"
+    version "0.0.4"
     license "MIT"
 
     depends_on "go" => :build
